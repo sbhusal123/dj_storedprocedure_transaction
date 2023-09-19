3b052ff1d2ecf9087ba7c1bcc425b49bd74b9a4a
Surya Bhusal <surya@fetch.ly>
2023-09-19 10:52:31 +0545

    Initialized app

--- booking/booking/settings.py
+++ booking/booking/settings.py
@@ -43,6 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # custom apps
+    'ticket',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
