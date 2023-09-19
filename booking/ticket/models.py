3e8bcc915d3baba16e4925c47681acb45387b2c3
Surya Bhusal <surya@fetch.ly>
2023-09-19 10:49:48 +0545

    Initialized models and configurations.

---
+++ booking/ticket/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    time = models.TimeField()
+    date = models.DateField()
+    seats_available = models.PositiveIntegerField()
+
+
+class Booking(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    customer_email = models.EmailField()
